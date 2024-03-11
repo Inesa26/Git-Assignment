5f942120a88e58607d0f6dabecfe3e96628caa87
Inesa Godorozea <Inesagodorozea@gmail.com>
2024-03-11 14:04:11 +0200

    Add Person class

---
+++ Test_Application/Test_Application/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Application
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}, Age: {Age}");
+        }
+    }
+}
